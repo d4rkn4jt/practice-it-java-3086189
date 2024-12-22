23c5f171379374ddba76beda558b6c8a3326c9f9
d4rkn4jt <129614209+d4rkn4jt@users.noreply.github.com>
2024-12-22 18:48:20 +0000

    Klasy i metody

--- _02_04/Ticket.java
+++ _02_04/Ticket.java
@@ -13,7 +13,31 @@ public class Ticket {
   // Add three methods to set the value of each field, called setDestination,
   // setPrice and setIsReturn.
 
+  public void setDestination(String destination) {
+    this.destination = destination;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public viod setIsReturn(boolean isReturn) {
+    this.isReturn = isReturn;
+  }
+
   // Add a separate method to get the value of each field, called getDestination,
   // getPrice and getIsReturn.
+
+  public String getDestination() {
+    return destination;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public boolean getIsReturn() {
+    return isReturn;
+  }
 
 }
